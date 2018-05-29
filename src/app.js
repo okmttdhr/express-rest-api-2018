05f4a68bfdd4dded4a5e578b33157c7ddc071211
okmttdhr <10101114fin@gmail.com>
2018-05-29 20:02:54 +0900

    add app.js

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// @flow
+import type {NextFunction, $Request, $Response} from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import router from './router';
+import {applyAirbrake} from './services';
+
+export const app = express();
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('combined'));
+}
+
+app.use((req: $Request, res: $Response, next: NextFunction) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
+
+app.use(applyAirbrake);
+
+app.use('/', router);
