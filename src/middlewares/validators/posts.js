cc171f97e4effbb326dd016dbe66f8ec0d0a595d
okmttdhr <10101114fin@gmail.com>
2018-06-02 12:07:10 +0900

    add parameter validation

---
+++ src/middlewares/validators/posts.js
@@ -0,0 +1,68 @@
+// @flow
+import {checkSchema} from 'express-validator/check/index';
+
+const request = {
+  title: {
+    errorMessage: 'Title is required.',
+    exists: true,
+    isEmpty: false,
+  },
+  body: {
+    errorMessage: 'Body is required.',
+    exists: true,
+    isEmpty: false,
+  },
+};
+
+const id = {
+  id: {
+    errorMessage: 'ID must be a number.',
+    isInt: true,
+    toInt: true,
+  },
+};
+
+const create = checkSchema(request);
+
+const update = checkSchema({
+  ...request,
+  ...id,
+});
+
+const detail = checkSchema(id);
+
+const index = checkSchema({
+  page: {
+    errorMessage: 'Page must be a number.',
+    optional: true,
+    isInt: true,
+    toInt: true,
+  },
+  per_page: {
+    errorMessage: 'Per-page must be a number.',
+    optional: true,
+    isInt: true,
+    toInt: true,
+  },
+  order_key: {
+    errorMessage: 'Invalid Order-key.',
+    optional: true,
+    isIn: {
+      options: [['title', 'body']],
+    },
+  },
+  order: {
+    errorMessage: 'Invalid Order.',
+    optional: true,
+    isIn: {
+      options: [['desc', 'asc']],
+    },
+  },
+});
+
+export default {
+  create,
+  update,
+  detail,
+  index,
+};
